34b2aaab4858aa59bb0e69f07199a0792484fe3f
Sheharyar <94967367+sheri0441@users.noreply.github.com>
2024-06-09 01:36:31 +0500

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,7 +7,7 @@ const Card = ({ content }) => {
       className="block rounded-lg overflow-hidden shadow-xl hover:shadow-2xl hover:scale-110 transition-all dark:bg-darkG bg-whiteN w-80"
     >
       <div className="w-full h-40">
-        <img className="w-full h-full object-cover" src={content.flag} alt="" />
+        <img className="w-full h-full object-cover" src={content.flag} alt={content.name} />
       </div>
       <div className="px-6 py-8 text-left text-black dark:text-whiteN">
         <h3 className="text-lg my-2 font-extrabold">{content.name}</h3>
